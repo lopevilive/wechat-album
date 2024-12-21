ce8716f07e10338336e600837900e11aa9d48825
zizhao <854945300@qq.com>
2024-12-21 19:56:51 +0800

    feat: 更新代码

--- pages/login/login.js
+++ pages/login/login.js
@@ -8,19 +8,26 @@ Page({
       wx.showLoading({mask: true})
       wx.removeStorageSync('token')
       let {src_path} = options
-      if (src_path) src_path = decodeURIComponent(src_path)
-      const {globalData: {web_src}} = getApp();
-      const token = await util.getToken()
       if (src_path) {
-        const instance = new util.UrlTools(src_path)
-        instance.addQuery({token})
-        src_path = instance.getFullpath()
-        const src = `${web_src}${src_path}`
-        const url = `../h5-view/h5View?web_src=${encodeURIComponent(src)}`
-        await wx.redirectTo({url})
+        wx.reLaunch({url: `../index/index?src_path=${encodeURIComponent(src_path)}`})
       } else {
-        await wx.reLaunch({url: '../index/index'})
+        wx.reLaunch({url: `../index/index?src_path=${encodeURIComponent('/')}`})
       }
+
+
+      // if (src_path) src_path = decodeURIComponent(src_path)
+      // const {globalData: {web_src}} = getApp();
+      // const token = await util.getToken()
+      // if (src_path) {
+      //   const instance = new util.UrlTools(src_path)
+      //   instance.addQuery({token})
+      //   src_path = instance.getFullpath()
+      //   const src = `${web_src}${src_path}`
+      //   const url = `../h5-view/h5View?web_src=${encodeURIComponent(src)}`
+      //   await wx.redirectTo({url})
+      // } else {
+      //   await wx.reLaunch({url: '../index/index'})
+      // }
     } catch(e) {
       console.error(e)
       await wx.reLaunch({url: '../index/index'})
